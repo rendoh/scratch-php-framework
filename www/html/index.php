59be8b7e216a3670e20e3bcea1fe31fb2425f4ab
Ryohei Endo <r-endo@studio-spoon.co.jp>
2020-01-09 19:11:42 +0900

    Add Router

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,17 @@
 <?php
 
 require '../bootstrap.php';
+
+use core\Router;
+use core\Request;
+
+$request = new Request();
+
+$router = new Router([
+  '/users' => ['controller' => 'foo'],
+  '/users/detail' => ['controller' => 'foo'],
+  '/users/detail/:id' => ['controller' => 'foo'],
+]);
+
+$result = $router->resolve($request->getPathInfo());
+var_dump($result);
